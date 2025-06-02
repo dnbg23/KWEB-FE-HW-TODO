3d539864be620ab1a01f598f250449605060d742
Diva Nasyantri Bambang Gunawan <divanasyantribg@gmail.com>
2025-06-02 16:06:23 +0900

    add

--- todo-hw/main.js
+++ todo-hw/main.js
@@ -1,11 +1,20 @@
 const input = document.getElementById('todo-input');
 const list = document.getElementById('todo-list');
 
+//add on enter
 input.addEventListener('keydown', function (e) {
   if (e.key === 'Enter' && input.value.trim()) {
     addTodo(input.value.trim());
     input.value = '';
   }
+});
+
+//add on button click
+addBtn.addEventListener('click', function(){
+    if (input.value.trim()) {
+        addTodo(input.value.trim());
+        input.value = '';
+    }
 });
 
 function addTodo(text) {
@@ -16,6 +25,7 @@ function addTodo(text) {
   checkbox.textContent = '⬜'; // unchecked emoji
   checkbox.style.cursor = 'pointer';
   checkbox.style.marginRight = '8px';
+  checkbox.title = 'Complete Task'
 
   taskText.textContent = text;
 
@@ -24,6 +34,7 @@ function addTodo(text) {
     const completed = checkbox.textContent === '☑️';
     checkbox.textContent = completed ? '⬜' : '☑️';
     taskText.style.textDecoration = completed ? 'none' : 'line-through';
+    checkbox.title = completed ? 'Complete Task' : 'Uncheck Task'
   };
 
   li.appendChild(checkbox);
